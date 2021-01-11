a2fac09d37b43731b82e7bbc89d43d4aed63a8be
Akerlin <56051184+Angop@users.noreply.github.com>
2021-01-11 11:58:06 -0800

    Part 3 complete

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Table from './Table'
 
 
 class App extends Component {
-	  render() {
-		      const characters = [
-			            {
+	  state = {
+        characters: [
+           {
 					            name: 'Charlie',
 					            job: 'Janitor',
 					          },
@@ -21,15 +21,35 @@ class App extends Component {
 			            {
 					            name: 'Dennis',
 					            job: 'Bartender',
-					          },
-			          ]
+                     },
+        ]
+     }
 
-		      return (
-			            <div className="container">
-			              <Table characterData={characters} />
-			            </div>
-			          )
-		    }
+
+   removeCharacter = index => {
+        const { characters } = this.state
+
+        this.setState({
+               characters: characters.filter((character, i) => {
+                        return i !== index
+                      }),
+             })
+   }
+
+
+
+   render() {
+        const { characters } = this.state
+
+        return (
+               <div className="container">
+                 <Table characterData={characters} removeCharacter={this.removeCharacter} />
+               </div>
+             )
+   }
+
+
 }
+
 
 export default App
